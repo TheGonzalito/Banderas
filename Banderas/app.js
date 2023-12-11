16d2f6d25e37034f0825352456e808c95b7852f1
TheGonzalito <153026764+TheGonzalito@users.noreply.github.com>
2023-12-11 18:32:04 -0300

    Update app.js

--- Banderas/app.js
+++ Banderas/app.js
@@ -1,7 +1,11 @@
 quest = ["Cual es la bandera de argentina?", "Cual es la bandera de brasil?", "Cual es la bandera de peru?", "Cual es la bandera de canada?"];
 banderas = ["argentina", "brasil", "peru", "canada"];
 aleatorioid = ["bandera","bandera1"];
-imagenbanderas = ["imgbandera","imgbandera1"];
+
+max = 1;
+min = 0;
+numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+numeroAleatorio1 = Math.floor(Math.random() * (max - min + 1)) + min;
 
 numaleatorio = Math.floor(Math.random() * quest.length);
 numaleatorio1 = Math.floor(Math.random() * banderas.length);
@@ -9,11 +13,24 @@ numaleatorio1 = Math.floor(Math.random() * banderas.length);
 //numaleatorioid1 = Math.floor(Math.random() * aleatorioid.length);
 
 
-var imgbandera = document.getElementById("bandera");
-var imgbandera1 = document.getElementById("bandera1");
+var imgbandera = document.getElementById(aleatorioid[numeroAleatorio]);
+var imgbandera1 = document.getElementById(aleatorioid[numeroAleatorio1]);
 var texto = document.getElementById("pregunta");
 
+idbandera = imgbandera.getAttribute('id');
+idbandera1 = imgbandera1.getAttribute('id');
+console.log( "Primer ID " + idbandera);
+console.log( "Primer ID " + idbandera1);
 
+if (imgbandera1.getAttribute('id') == imgbandera.getAttribute('id')){
+    while (imgbandera1.getAttribute('id') == imgbandera.getAttribute('id')){
+        numeroAleatorio1 = Math.floor(Math.random() * (max - min + 1)) + min;
+        var imgbandera1 = document.getElementById(aleatorioid[numeroAleatorio1]);
+    }
+}
+
+console.log(idbandera);
+console.log(idbandera1);
 
 questAleatoria = quest[numaleatorio];
 texto.innerHTML = questAleatoria;
